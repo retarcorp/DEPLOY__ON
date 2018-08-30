9365fa9a14a390318ae08c153a9feed8b1cba78a
mmfKupl <ikuplevich@mail.ru>
2018-08-31 01:16:22 +0300

    reg link bug fixed

--- pages/Registratsia/index.vue
+++ pages/Registratsia/index.vue
@@ -11,7 +11,7 @@
                 :class="{'button--disabled': !isFormValid}"
                 :disabled="!isFormValid">Регистрация</button>
         </div>
-        <p class="form__question">Уже есть аккаунт? <nuxt-link to="/Registratsia" class="link">Войти</nuxt-link></p>
+        <p class="form__question">Уже есть аккаунт? <nuxt-link to="/Vhod" class="link">Войти</nuxt-link></p>
     </form>
 </template>
 
